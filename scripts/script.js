cb29bf2247fdf78a36f5977467a054cb267f0fb5
Shahyn <shahynkamali@gmail.com>
2019-02-15 16:56:38 -0500

    created basic css framework

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,69 @@
-$(document).ready(function () {
-    $(".yes").hide();
-    $(".no").hide();
+
+
+const angryGordy = (answerHTML) =>{
+    $('.answer').html(answerHTML);
+    
+}
+
+const initGame = (randomItem) =>{
     $(".pickChoice").click(function (e) {
         e.preventDefault();
-        const randomItem = foodArray[Math.floor(Math.random() * foodArray.length)];
+         randomItem = foodArray[Math.floor(Math.random() * foodArray.length)];
         const itemHTML = `
         <div class="randomItem">
             <h2>${randomItem.name}</h2>
             <img src="${randomItem.img}"
         </div>`
         $('.item').html(itemHTML);
-        $(".yes").show();
-        $(".no").show();
-        $(".yes").click(function (e) {
-            e.preventDefault();
-            const answerHTML = `
-            <div class="answerSection">
-                <p>${randomItem.answer}</p>
-            </div>
-            <div class="image">
-                <img src="./images/gordy.jpeg">
-            </div>
-            `
-            $('.answer').html(answerHTML);
-        });
-        $(".no").click(function (e) {
-            e.preventDefault();
-            const answerHTML = `
-            <div class="answerSection">
-                <p>${randomItem.falseAnswer}</p>
-            </div>`
-            $('.answer').html(answerHTML);
-
-        });
+        $('.choose').addClass('flex');
+        $('.choose').show();
+        displayChoice(randomItem);
     });
-});
+}
+
+const displayChoice = (randomItem, answerHTML) =>{
+    $(".yes").click(function (e) {
+        e.preventDefault();
+        answerHTML = `
+        <div class="answerSection">
+            <p>${randomItem.answer}</p>
+        </div>
+        <div class="image">
+            <img src="./images/gordy.jpeg">
+        </div>
+        `
+        restart();
+        angryGordy(answerHTML);
+        
+    });
+    
+    $(".no").click(function (e) {
+        e.preventDefault();
+        const answerHTML = `
+        <div class="answerSection">
+            <p>${randomItem.falseAnswer}</p>
+            
+        </div>
+        <div class="image">
+            <img src="./images/angel.png">
+        </div>`
+        restart();
+        angryGordy(answerHTML);
+    });
+}
+
+const restart = () =>{
+    $('.choose').hide();
+    $('.item').hide();
+    $('.pickChoice').hide();
+    $('.restart').show();
+    $('.restart').click(function () {
+    location.reload();
+    });
+        
+}
+
+
+        $(function () {
+            initGame();
+        });
